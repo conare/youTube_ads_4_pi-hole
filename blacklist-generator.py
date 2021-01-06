e704849b7ac2dfeb78c8622939e5db1389c0373d
chris onare <cw.onare@gmail.com>
2021-01-06 18:39:11 +1100

    update range to include 20

--- blacklist-generator.py
+++ blacklist-generator.py
@@ -8,13 +8,13 @@ fingerprints = [
   'v2u0n-hxad'
 ]
 for fingerprint in fingerprints:
-  for i in range(0,20):
+  for i in range(0,21):
     # generate blacklist domains
     blacklist.append(f'r{i}---sn-{fingerprint}.googlevideo.com')
     blacklist.append(f'r{i}.sn-{fingerprint}.googlevideo.com')
 
 with open("black.list", "a") as outfile:
     outfile.write("\n".join(blacklist))
-    
+
 with open("youtubelist.txt", "a") as outfile:
     outfile.write("\n".join(blacklist))
